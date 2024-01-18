ceb76d9c14f179b04c4420852d4c461eeaa5041e
vaishnavi <vaishnavi@ucsb.edu>
2024-01-17 19:14:11 -0800

    vaishnavi's hello world app

---
+++ vaishnavi_hello_world/README.md
@@ -0,0 +1,4 @@
+## Software needed
+* Install Node.js
+* Run `npm start` to deploy on local host
+* deployed on: https://cs148-helloworld.vercel.app/
---
+++ vaishnavi_hello_world/src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
