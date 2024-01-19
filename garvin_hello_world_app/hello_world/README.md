b680c85313b7225d34ad0aa70ec5663d5d66f0af
garvinwyoung <garvinwyoung@ucsb.edu>
2024-01-19 02:17:25 -0800

    lab01 commit #1

---
+++ garvin_hello_world_app/hello_world/README.md
@@ -0,0 +1,4 @@
+## Software Needed
+Install/Update Node.js, npm, vercel, 
+Run `npm start` to deploy on local host
+Deployed Link: https://cs148-hello-world-app.vercel.app/
---
+++ garvin_hello_world_app/hello_world/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App-header">
+        Hello World :) <br />
+        <span style={{ fontSize: 'smaller' }}>
+          Welcome to the MedMinder Web App
+        </span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
