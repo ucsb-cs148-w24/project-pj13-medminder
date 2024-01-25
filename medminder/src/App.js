59c0290b91561575389ed0f2e1c7e3f63986bbb4
Tim Choi <tchoi@ucsb.edu>
2024-01-25 13:18:48 -0800

    tested sending data to firebase

--- medminder/src/App.js
+++ medminder/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SignIn from "./components/SignIn";
+import SimpleDataSender from "./components/StoreTestData";
 
 function App() {
   return (
     <div className="App">
       <SignIn />
+      <SimpleDataSender />
     </div>
   );
 }
