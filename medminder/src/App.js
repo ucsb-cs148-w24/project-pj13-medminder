35a5d19c54a4cf553d7e78adcea46ec46518507e
Vaishnavi Himakunthala <vaishnavi.himakunthala@gmail.com>
2024-01-25 14:44:29 -0800

    displays info from database once get Data button is clicked

--- medminder/src/App.js
+++ medminder/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SignIn from "./components/SignIn";
-
+import Display from "./components/data-show";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <SignIn/>
+      <Display/>
     </div>
   );
 }
