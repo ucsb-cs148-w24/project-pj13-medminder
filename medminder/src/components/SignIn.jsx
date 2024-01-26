390836e853a34a83dbf3756bfe7d867301bbd157
Michael Cheng <michaelcheng@ucsb.edu>
2024-01-26 13:18:24 -0800

    mc - save new user info to database after Google OAuth

--- medminder/src/components/SignIn.jsx
+++ medminder/src/components/SignIn.jsx
@@ -1,9 +1,35 @@
-import { signInWithGooglePopup } from "../utils/firebase.utils"
+import { signInWithGooglePopup } from "../utils/firebase.utils";
+import { getDatabase, ref, query, get, set, orderByChild, equalTo } from 'firebase/database';
 
 const SignIn = () => {
-    const logGoogleUser = async () => {
+    const logGoogleUser = async (name) => {
         const response = await signInWithGooglePopup();
         console.log(response);
+
+        const userId = response.user.uid;
+        const database = getDatabase();
+        const usersRef = ref(database, 'Users');
+        const nameQuery = query(usersRef, orderByChild('Name'), equalTo(response.user.displayName));
+
+        const snapshot = await get(nameQuery);
+
+        if (snapshot.exists()) {
+            console.log('Returning User.');
+        } else {
+            console.log('New User.');
+            const userData = {
+                Name: response.user.displayName,
+                //email: response.user.email,
+                Sex: "X",
+                Age: 24,
+                DOB: "01/01/2000",
+                // default values, to be updated later
+            };
+      
+            // Store user data under "Users/{userId}"
+            const userRef = ref(database, `Users/${userId}`);
+            set(userRef, userData);
+        }
     }
 
     return (
