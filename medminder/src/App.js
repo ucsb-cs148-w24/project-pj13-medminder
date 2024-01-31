0875507b8c6c81ee906220b80b45b8143ff68329
Vaishnavi Himakunthala <vaishnavi.himakunthala@gmail.com>
2024-01-31 12:38:50 -0800

    resolved all merge conflicts

--- medminder/src/App.js
+++ medminder/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import AddEventButton from './components/AddEventButton';
 import SignIn from "./components/SignIn";
 import Display from "./components/data-show";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import SimpleDataSender from "./components/StoreTestData";
 import AlertPopup from "./components/AlertPopup/AlertPopup.jsx"
 
