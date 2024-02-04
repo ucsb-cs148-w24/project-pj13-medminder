c64530e65fa39de9589b042207697df70ed55da0
HansonYu23 <hansonyu@ucsb.edu>
2024-02-04 15:20:40 -0800

    initial commit, data being displayed from database and being displayed based on day of week

---
+++ medminder/src/components/Alert.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Alert = ({alert}) => {
+
+    const styles = {
+        container: {
+          padding: '0px',
+          margin: '15px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        detail: {
+          margin: '0 50px',
+          fontSize: '17px',
+          fontFamily: 'Bahnschrift',
+        },
+        button: {
+          margin: '0 5px',
+          fontSize: '15px',
+          fontFamily: 'Bahnschrift',
+        }
+      };
+
+
+      // TODO: implement edit and delete button functionality based on key of Alert component
+    return (
+        <div style={styles.container}>
+            <p style={styles.detail}>{alert.medicine}</p>
+            <p style={styles.detail}>{alert.dosage}</p>
+            <p style={styles.detail}>{alert.time}</p>
+            <button style={styles.button}>Edit</button>
+            <button style={styles.button}>Delete</button>
+        </div>
+    );
+};
+
+export default Alert;
--- medminder/src/pages/dashboard.js
+++ medminder/src/pages/dashboard.js
@@ -1,17 +1,14 @@
 import AlertPopupWithButton from "../components/AlertPopupWithButton/AlertPopupWithButton.jsx"
-import Display from "../components/data-show.jsx";
 import SimpleDataSender from "../components/StoreTestData.jsx";
 import DateNavigator from "../components/SetDateOnMain.jsx"
 import { BrowserRouter, Navigate } from "react-router-dom";
 function Dashboard() {
 
   return (
-    <div className="Dasboard">
+    <div className="Dasboard" >
       <AlertPopupWithButton/>
       <SimpleDataSender />
-      <DateNavigator />
-      <Display/>
-  
+      <DateNavigator />  
     </div>
   );
 }
