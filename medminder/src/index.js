78322ea66df6fd6a47e14955232be3b24ba99bcf
Michael Cheng <michaelcheng@ucsb.edu>
2024-02-03 16:01:15 -0800

    fix warning because process.env.CI = true -> no warnings allowed in deployment build

--- medminder/src/index.js
+++ medminder/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { UserIdProvider } from './components/UserIdContext';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <GoogleOAuthProvider clientId="48920437462-2209kti0vlp0sv5tq72egns0hm2shocg.apps.googleusercontent.com">
