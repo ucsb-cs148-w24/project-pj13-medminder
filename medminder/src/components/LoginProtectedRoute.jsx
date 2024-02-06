4b05f6681bff90c4cf885a3606a54e59c64a8ed3
James Pflaging <pflagingjames@gmail.com>
2024-02-06 00:45:09 -0800

    fixed race codition and pulled out into authcontext separate for simplicity

--- medminder/src/components/LoginProtectedRoute.jsx
+++ medminder/src/components/LoginProtectedRoute.jsx
@@ -1,14 +1,10 @@
-import { Routes, Route, Link, Navigate, Outlet, OutletProps} from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
-import { browserLocalPersistence, getAuth, onAuthStateChanged, signOut} from "firebase/auth";
-import { useEffect, useState } from 'react';
-import { useAuthContext, useUserId } from './UserIdContext';
+import { Navigate, Outlet} from 'react-router-dom';
+import { useAuthContext } from './AuthContext.js';
 
 
 
 const ProtectedRoute = ({ fallback }) => {
-
-    return useAuthContext ? <Outlet /> : <Navigate to='/'/>;
+    return useAuthContext().currentUser ? <Outlet /> : <Navigate to='/'/>;
 };
 
 export default ProtectedRoute
--- medminder/src/components/UserIdContext.js
+++ medminder/src/components/UserIdContext.js
@@ -1,29 +1,16 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { onAuthStateChanged, getAuth } from 'firebase/auth';
 
 export const UserIdContext = createContext();
-const AuthContext = createContext();
 
 export const UserIdProvider = ({ children }) => {
   const [userId, setUserId] = useState(null);
-  const [currentUser, setCurrentUser] = useState();
-  // could use some implementation of loading state to prevent some weirdness
-  const auth = getAuth()
 
   const updateUserId = (newUserId) => {
     setUserId(newUserId);
   };
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-        setCurrentUser(user)
-    });
-
-    return () => unsubscribe();
-  }, [auth]);
-
   return (
-    <UserIdContext.Provider value={{ userId, updateUserId }}>
+    <UserIdContext.Provider value={{ userId, updateUserId}}>
       {children}
     </UserIdContext.Provider>
   );
@@ -36,11 +23,3 @@ export const useUserId = () => {
   }
   return context;
 };
-
-export const useAuthContext = () => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error('useAuthContext must be used within a UserIdProvider');
-  }
-  return context;
-}
