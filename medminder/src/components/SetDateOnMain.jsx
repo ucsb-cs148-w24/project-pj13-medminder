6d990b25a7c20d5788f25d11587ad8ea14c8df29
Michael Cheng <michaelcheng@ucsb.edu>
2024-02-07 12:22:32 -0800

    botton to jump to current day, move prev/next day buttons to lhs/rhs of date

--- medminder/src/components/SetDateOnMain.jsx
+++ medminder/src/components/SetDateOnMain.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
 import DataDisplay from './MedicineAlerts';
+import './styles.css';
+
+const dateNavigatorStyle = {
+    display: 'flex',
+    justifyContent: 'center', // Center the content horizontally
+    alignItems: 'center', // Center the content vertically
+};
+  
+const buttonStyle = {
+    margin: '0 30px', // Add margin for spacing between buttons and date
+};
 
 const dateStyle = {
     textAlign: 'center', // Center the text
     fontSize: '100px',    // Increase font size
+    width: '540px',
+    height: '130px',
     marginTop: '0%',    // Adjust vertical positioning (optional)
 };
 
@@ -16,16 +29,30 @@ const DateNavigator = () => {
         setCurrentDate(newDate);
     };
 
+    const goToToday = () => {
+        setCurrentDate(new Date());
+    };
+
     const formatDate = (date) => {
         return date.toLocaleDateString();
     };
 
     return (
         <div style={{ textAlign: 'center' }}> {/* Center the entire content */}
-        <button onClick={() => changeDate(-1)}>Previous Day</button>
-        <h2 style={dateStyle}>{formatDate(currentDate)}</h2> {/* Apply the styles to the date */}
-        <button onClick={() => changeDate(1)}>Next Day</button>
-        <DataDisplay date={currentDate.getDay()} />
+            <div style={dateNavigatorStyle}>
+                <button style={buttonStyle} onClick={() => changeDate(-1)}>
+                    Previous Day
+                </button>
+                <h2 style={dateStyle}>{formatDate(currentDate)}</h2>
+                <button style={buttonStyle} onClick={() => changeDate(1)}>
+                    Next Day
+                </button>
+                
+            </div>
+            <button className="todayButton" onClick={goToToday}>
+                Today
+            </button>
+            <DataDisplay date={currentDate.getDay()} />
         </div>
     );
 };
