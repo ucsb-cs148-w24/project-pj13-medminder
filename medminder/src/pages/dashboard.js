a0239a7608f3827de9ccf0cafef3b29cab021770
James Pflaging <pflagingjames@gmail.com>
2024-02-07 16:43:33 -0800

    removing unceessary import

--- medminder/src/pages/dashboard.js
+++ medminder/src/pages/dashboard.js
@@ -1,5 +1,4 @@
 import AlertPopupWithButton from "../components/AlertPopupWithButton/AlertPopupWithButton.jsx"
-import SimpleDataSender from "../components/StoreTestData.jsx";
 import DateNavigator from "../components/SetDateOnMain.jsx"
 import MedicationReminder from '../components/MedicationReminder.jsx';
 import SignOut from "../components/SignOut.jsx";
