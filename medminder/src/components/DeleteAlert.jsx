2ce2f5bb23e5cb92023c8b39c75bd3bfe7d761d3
Timothy Choi <tim@169-231-118-196.wireless.ucsb.edu>
2024-02-21 14:52:07 -0800

    added a confirmation popup before deletion

--- medminder/src/components/DeleteAlert.jsx
+++ medminder/src/components/DeleteAlert.jsx
@@ -6,22 +6,27 @@ import { AiFillRest } from "react-icons/ai";
 import '../Dash-style.css';
 
 export default function DeleteAlert(props) {
-
     const auth = useAuthContext();
     const userId = auth.currentUser.uid;
     const dataRef = ref(database, 'Users/' + userId + '/UserData/' + props.timestamp);
 
     const deleteAlert = () => {
-    remove(dataRef)
-        .then(() => {
-        console.log('Data deleted successfully');
-        })
-        .catch((error) => {
-        console.error('Error deleting data:', error);
-        });
+        // Display a confirmation dialog
+        const isConfirmed = window.confirm('Are you sure you want to remove this medicine?');
+        
+        // If the user confirms, proceed with the delete operation
+        if (isConfirmed) {
+            remove(dataRef)
+                .then(() => {
+                    console.log('Data deleted successfully');
+                })
+                .catch((error) => {
+                    console.error('Error deleting data:', error);
+                });
+        }
     }
 
     return (
         <button className="delete" onClick={deleteAlert}><AiFillRest/></button>
-    )
+    );
 }
