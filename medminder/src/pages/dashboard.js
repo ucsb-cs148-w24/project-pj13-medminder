3f31abc9e2ae0c53249b2257f76a705eec2b4c7e
Timothy Choi <tim@169-231-125-34.wireless.ucsb.edu>
2024-02-26 13:59:14 -0800

    added header to dashboard

--- medminder/src/pages/dashboard.js
+++ medminder/src/pages/dashboard.js
@@ -1,16 +1,23 @@
-import AlertPopupWithButton from "../components/AlertPopupWithButton/AlertPopupWithButton.jsx"
-import DateNavigator from "../components/SetDateOnMain.jsx"
+import React from 'react';
+import AlertPopupWithButton from "../components/AlertPopupWithButton/AlertPopupWithButton.jsx";
+import DateNavigator from "../components/SetDateOnMain.jsx";
 import SignOut from "../components/SignOut.jsx";
 
 import '../Dash-style.css';
-function Dashboard() {
 
+function Dashboard() {
   return (
-    <div>
-      <SignOut/>
-      <div className="nav" >
-       </div>
-      <div className="Dasboard" style={{textAlign: 'center', marginTop: '60px'}}>
+    <div className="dashboard-container">
+      <header className="dashboard-header">
+        <div className="header-content">
+          <h1>Medminder</h1> {/* Header text added here */}
+        </div>
+        <SignOut />
+      </header>
+      <div className="nav">
+        {/* Navigation content if any */}
+      </div>
+      <div className="dashboard-main" style={{textAlign: 'center', marginTop: '60px'}}>
         <DateNavigator />
         <AlertPopupWithButton TextInButton={"+"} buttonDesign={"button1"}/>
       </div>
