ccd119b696db3af060f1e125f6609fb17c80cda6
Michael Cheng <michaelcheng@ucsb.edu>
2024-03-06 13:36:33 -0800

    switch functional

--- medminder/src/components/EmailToggle.jsx
+++ medminder/src/components/EmailToggle.jsx
@@ -1,18 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from "react";
+import { useAuthContext } from "./AuthContext";
+import { getDatabase, ref, get, set } from 'firebase/database';
 import "@material/react-switch/dist/switch.css";
-import Switch from '@material/react-switch';
+import Switch from "@material/react-switch";
 
 const EmailToggle = () => {
-    const [checked, setChecked] = useState(false);
+    const auth = useAuthContext();
+    const userID = auth.currentUser.uid;
+    const database = getDatabase();
+    const dataRef = ref(database, `Users/${userID}/UserPref/Email`);
+    const [checked, setChecked] = useState(true);
+
+    useEffect(() => {
+        // Fetch the initial value when the component mounts
+        const fetchPrefs = async () => {
+            try {
+                const notifVal = (await get(dataRef)).val();
+                setChecked(notifVal);
+            } catch (error) {
+                console.error("Error fetching email notifications:", error);
+            }
+        };
+
+        fetchPrefs();
+    });
+
+    const handleToggle = async () => {
+        try {
+            // Toggle the value
+            const newVal = !checked;
+
+            // Update value in the database
+            await set(dataRef, newVal);
+
+            // Update the local state
+            setChecked(newVal)
+
+            // console.log("Email Notifications toggled successfully.");
+        } catch (error) {
+            console.error("Error toggling email notifications:", error);
+        }
+    };
 
     return (
         <React.Fragment>
             <Switch
-                nativeControlId='toggle'
+                nativeControlId="toggle"
                 checked={checked}
-                onChange={(e) => setChecked(e.target.checked)}
+                onChange={handleToggle}
             />
-            <label htmlFor='toggle' classname="email-switch">Email Notifications</label>
+            <label htmlFor="toggle">Email Notifications</label>
         </React.Fragment>
     );
 
