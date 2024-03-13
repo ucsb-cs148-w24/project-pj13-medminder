7f3e13e42bd781db04a9b38a9b1b204352c6c8d0
Michael Cheng <michaelcheng@ucsb.edu>
2024-03-13 15:14:52 -0700

    tour v1.1

--- medminder/src/steps.js
+++ medminder/src/steps.js
@@ -2,39 +2,19 @@
 export const steps = [
     {
         selector: ".dashboard-container",
-        content: "Welcome to a Quick Tour of Medminder!",
+        content: "Welcome to a quick tour of medminder!",
     },
     {
         selector: ".button1",
-        content: "Add a New Alert",
-    },
-    // {
-    //     selector: ".modal-content",
-    //     content: "Fill Out a Name, Dosage, and Time to create an alert",
-    // },
-    // {
-    //     selector: ".submit-modal",
-    //     content: "Press submit to create!",
-    // },
-    {
-        selector: ".edit",
-        content: "Edit an alert",
+        content: "Add a new alert",
     },
     {
-        selector: ".delete",
-        content: "Delete an alert",
+        selector: ".complete-button",
+        content: "Mark an Alert as Complete",
     },
     {
-        selector: ".complete",
-        content: "Mark an alert as completed",
-    },
-    {
-        selector: ".info",
-        content: "View common side effects",
-    },
-    {
-        selector: ".addToCalendar",
-        content: "Export an alert to Google Calendar",
+        selector: ".more-actions",
+        content: "View common side effects, edit/delete an alert, or push to Google Calendar",
     },
     {
         selector: ".arrow-left",
@@ -49,12 +29,12 @@ export const steps = [
         content: "Jump to the current day",
     },
     {
-        selector: ".notifButton",
-        content: "Toggle email notifications",
-    },
-    {
         selector: ".new-profile",
         content: "Create a new profile for a family member or patient",
+    },
+    {
+        selector: ".notifButton",
+        content: "Toggle email notifications",
     },
     {
         selector: ".sign-out",
