c8d6751145e895e0dc2d704efa79fd9e5d8446ea
James Pflaging <pflagingjames@gmail.com>
2024-03-15 19:41:12 -0700

    replacing all alerts except for the delete which takes input

--- medminder/src/components/AlertPopupWithButton/formHandlers.js
+++ medminder/src/components/AlertPopupWithButton/formHandlers.js
@@ -1,14 +1,45 @@
 // formHandlers.js
 import { ref, set } from 'firebase/database';
 import { database } from '../../utils/firebase.utils'; // Ensure this path is correct
+import Toastify from 'toastify-js';
 
 export const handleSubmit = (formData, userId, clearForm, togglePopup, timestamp, userProfile) => {
     // Adjust the database reference to include the userId in the path
     const dataRef = ref(database, `Users/${userId}/${userProfile}/` + timestamp);
 
     set(dataRef, formData)
-        .then(() => alert('Alert has been successfully created!'))
-        .catch((error) => alert('Failed to send data: ' + error.message));
+        .then(() => 
+        Toastify({
+        text: "Alert created!",
+        duration: 3000,
+        newWindow: false,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", 
+        stopOnFocus: true, 
+        style: {
+        background: "#00b09b",
+        },
+        offset: {
+        y: 95
+        },
+    }).showToast())
+        .catch((error) => 
+        Toastify({
+        text: "Failed to send data.",
+        duration: 3000,
+        newWindow: false,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", 
+        stopOnFocus: true, 
+        style: {
+        background: "#b50505",
+        },
+        offset: {
+        y: 95
+        },
+    }).showToast());
 
     clearForm();
     togglePopup();
--- medminder/src/components/update.js
+++ medminder/src/components/update.js
@@ -1,5 +1,8 @@
 import { ref, set } from 'firebase/database';
 
+import Toastify from 'toastify-js';
+
+
 export const Update = (name, sex, age, dob, userId,email,  togglePopup, database, numProfile) => {
     const userRef = ref(database, `Users/${userId}/UserInfo`);
     const updateInfo = {
@@ -10,8 +13,36 @@ export const Update = (name, sex, age, dob, userId,email,  togglePopup, database
         DOB: dob,
         numProfiles: numProfile
         }
-    set(userRef, updateInfo).then(() => alert('Information updated!'))
-    .catch((error) => alert('Failed to send data: ' + error.message));
+    set(userRef, updateInfo).then(() => Toastify({
+        text: "Updated profile!",
+        duration: 3000,
+        newWindow: false,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", 
+        stopOnFocus: true, 
+        style: {
+        background: "#00b09b",
+        },
+        offset: {
+        y: 95
+        },
+    }).showToast())
+    .catch((error) => Toastify({
+        text: "Update failed!",
+        duration: 3000,
+        newWindow: false,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", 
+        stopOnFocus: true, 
+        style: {
+        background: "#b50505",
+        },
+        offset: {
+        y: 95
+        },
+    }).showToast());
     
     console.log("updated!")
     togglePopup()
